eb9dd01e07f17ac4e9f8658a0866f510ae9f33df
Asumi Saito <asumisaito1@gmail.com>
2024-11-18 13:51:27 -0500

    Update run.py

--- run.py
+++ run.py
@@ -2,3 +2,15 @@ a = 10
 b = 20
 c = a + b 
 print(c) 
+
+import urllib.request
+
+import rasterio
+
+
+urllib.request.urlretrieve(
+    "http://fatra.cnr.ncsu.edu/us-iale2017/US_elevation.tif", "elevation.tif"
+)
+
+with rasterio.open("elevation.tif") as dem:
+    print(dem.crs)
